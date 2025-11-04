da2878b43d980d7390c549b23ba278e0dfbeffa3
AkshatMekol <83553564+AkshatMekol@users.noreply.github.com>
2025-11-04 14:47:55 +0530

    Update mongo_utils.py

--- utils/mongo_utils.py
+++ utils/mongo_utils.py
@@ -7,7 +7,6 @@ tenders_collection = db[TENDERS_COLLECTION]
 vector_collection = db[VECTOR_COLLECTION]
 
 def get_tender_ids(min_value):
-    """Return a list of tender IDs with tender_value >= min_value."""
     cursor = tenders_collection.find(
         {"tender_value": {"$gte": min_value}},
         {"_id": 1}
@@ -15,12 +14,10 @@ def get_tender_ids(min_value):
     return [str(doc["_id"]) for doc in cursor]
 
 def insert_vectors(docs):
-    """Insert a list of documents into vector collection."""
     if docs:
         vector_collection.insert_many(docs)
 
 def document_exists_for_tender(tender_id: str, document_name: str) -> bool:
-    """Check if a document already exists for a tender."""
     existing = vector_collection.find_one({
         "tender_id": tender_id,
         "document_name": document_name
