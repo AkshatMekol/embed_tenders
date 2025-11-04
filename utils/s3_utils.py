bb52752785644ab3f19a50f1c6b19eb22c50a86d
AkshatMekol <83553564+AkshatMekol@users.noreply.github.com>
2025-11-04 14:45:38 +0530

    Update s3_utils.py

--- utils/s3_utils.py
+++ utils/s3_utils.py
@@ -2,7 +2,6 @@ import boto3
 from .config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, S3_BUCKET
 from io import BytesIO
 
-# ================= SYNCHRONOUS S3 CLIENT =================
 s3 = boto3.client(
     "s3",
     region_name=AWS_REGION,
@@ -10,8 +9,7 @@ s3 = boto3.client(
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY
 )
 
-# ================= LIST PDF FILES =================
-def list_pdfs_sync(prefix):
+def list_pdfs(prefix):
     """Return a list of PDF keys under the given S3 prefix."""
     pdf_keys = []
     paginator = s3.get_paginator("list_objects_v2")
@@ -22,8 +20,7 @@ def list_pdfs_sync(prefix):
                 pdf_keys.append(key)
     return pdf_keys
 
-# ================= DOWNLOAD PDF =================
-def download_pdf_sync(key):
+def download_pdf(key):
     """Download PDF from S3 and return as BytesIO."""
     obj = s3.get_object(Bucket=S3_BUCKET, Key=key)
     return BytesIO(obj['Body'].read())
