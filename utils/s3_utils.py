cfe656d6b8f864821b690efdeae61780567901a0
AkshatMekol <83553564+AkshatMekol@users.noreply.github.com>
2025-11-18 15:50:35 +0530

    Update s3_utils.py

--- utils/s3_utils.py
+++ utils/s3_utils.py
@@ -1,26 +1,24 @@
 import boto3
-from .config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, S3_BUCKET
 from io import BytesIO
+from .config import (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION)
 
-def list_s3_pdfs(bucket, prefix):
-    s3 = boto3.client("s3",
+def get_s3_client():
+    return boto3.client(
+        "s3",
         aws_access_key_id=AWS_ACCESS_KEY_ID,
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
         region_name=AWS_REGION
     )
 
+
+def list_s3_pdfs(bucket, prefix):
+    s3 = get_s3_client()
     objs = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
-    pdfs = []
+    return [o["Key"] for o in objs.get("Contents", []) if o["Key"].lower().endswith(".pdf")]
 
-    for obj in objs.get("Contents", []):
-        key = obj["Key"]
-        if key.lower().endswith(".pdf"):
-            pdfs.append(key)
 
-    print(f"PDFs found: {len(pdfs)}")
-    return pdfs
-
-def download_pdf(key):
-    obj = s3.get_object(Bucket=S3_BUCKET, Key=key)
-    return BytesIO(obj['Body'].read())
+def fetch_pdf(bucket, key):
+    s3 = get_s3_client()
+    obj = s3.get_object(Bucket=bucket, Key=key)
+    return BytesIO(obj["Body"].read())
 
