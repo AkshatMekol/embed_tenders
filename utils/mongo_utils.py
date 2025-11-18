7ad0e8746fab1aa54a298f8d3663115393f204b2
AkshatMekol <83553564+AkshatMekol@users.noreply.github.com>
2025-11-18 15:52:34 +0530

    Update mongo_utils.py

--- utils/mongo_utils.py
+++ utils/mongo_utils.py
@@ -1,25 +1,37 @@
+import gc
 from pymongo import MongoClient
-from .config import MONGO_URI, DB_NAME, TENDERS_COLLECTION, VECTOR_COLLECTION
+from sentence_transformers import SentenceTransformer
+from .config import MONGO_URI, DB_NAME, VECTOR_COLLECTION, TENDERS_COLLECTION, EMBEDDING_MODEL_NAME, device, BATCH_SIZE
 
-client = MongoClient(MONGO_URI)
-db = client[DB_NAME]
-tenders_collection = db[TENDERS_COLLECTION]
+mongo = MongoClient(MONGO_URI)
+db = mongo[DB_NAME]
 vector_collection = db[VECTOR_COLLECTION]
 
-def get_tender_ids(min_value):
-    cursor = tenders_collection.find(
-        {"tender_value": {"$gte": min_value}},
-        {"_id": 1}
-    )
-    return [str(doc["_id"]) for doc in cursor]
+model = SentenceTransformer(EMBEDDING_MODEL_NAME, device=device)
 
-def insert_vectors(docs):
-    if docs:
+def embed_and_upload_chunks(chunks, document_name, tender_id):
+    if not chunks:
+        return
+
+    for i in range(0, len(chunks), BATCH_SIZE):
+        batch = chunks[i:i + BATCH_SIZE]
+        texts = [c["data"] for c in batch]
+        embeddings = model.encode(texts, batch_size=BATCH_SIZE, show_progress_bar=False).tolist()
+
+        docs = []
+        for c, emb in zip(batch, embeddings):
+            docs.append({
+                "tender_id": tender_id,
+                "document_name": document_name,
+                "page": c["page"],
+                "position": c["position"],
+                "sub_position": c["sub_position"],
+                "type": c["type"],
+                "is_scanned": c["is_scanned"],
+                "text": c["data"],
+                "embedding": emb
+            })
+
         vector_collection.insert_many(docs)
 
-def document_exists_for_tender(tender_id: str, document_name: str) -> bool:
-    existing = vector_collection.find_one({
-        "tender_id": tender_id,
-        "document_name": document_name
-    })
-    return existing is not None
+    gc.collect()
