06f7dd75c78c170e7e2a02100654ef76c2b170e2
AkshatMekol <83553564+AkshatMekol@users.noreply.github.com>
2025-11-20 19:00:09 +0530

    Update mongo_utils.py

--- utils/mongo_utils.py
+++ utils/mongo_utils.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 from utils.config import MONGO_URI, DB_NAME, VECTOR_COLLECTION, TENDERS_COLLECTION
 
 mongo = MongoClient(MONGO_URI)
@@ -12,3 +13,16 @@ def store_embeddings_in_db(embeddings, document_name, tender_id):
         vector_collection.insert_many(embeddings)
     except Exception as e:
         print(f"❌ Mongo Insert Error: {e}")
+
+def get_tender_ids(min_value=4000000000):
+    query = {}
+    if min_value > 0:
+        query = {"tender_value": {"$gte": min_value}}
+
+    cursor = tenders_collection.find(query, {"_id": 1})
+
+    tender_ids = []
+    for doc in cursor:
+        tender_ids.append(str(doc["_id"]))
+
+    return tender_ids
