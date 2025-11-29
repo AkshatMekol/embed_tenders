a5f7f1537376c8d0179bc4f2781e5273c8ff0041
AkshatMekol <83553564+AkshatMekol@users.noreply.github.com>
2025-11-29 12:49:41 +0530

    Update pdf_processing.py

--- utils/pdf_processing.py
+++ utils/pdf_processing.py
@@ -1,10 +1,10 @@
 import gc
+import asyncio
 import pdfplumber
 from io import BytesIO
-import asyncio
 from utils.chunking import split_text_to_subchunks
-from utils.config import MAX_PROCESSES_DEEPSEEK, MAX_PROCESSES_GROQ, PDF_BATCH_SIZE
 from utils.regular_helpers import extract_page_content, elements_to_positions
+from utils.config import MAX_PROCESSES_DEEPSEEK, MAX_PROCESSES_GROQ, PDF_BATCH_SIZE
 from utils.scanned_helpers import is_scanned_page, process_scanned_page_worker, deepseek_translate_worker
 
 async def groq_worker(job, semaphore):
